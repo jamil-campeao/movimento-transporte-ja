dc0671dff52594d57dd0d56bd6210739d8bea2c1
Jamil Campeão <jamilcampeao@gmail.com>
2025-08-02 19:29:02 -0300

    fix: ajuste erro de import

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 # models.py
 from sqlalchemy import Column, Integer, String, LargeBinary, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 
 class Relato(Base):
     __tablename__ = "relatos"
