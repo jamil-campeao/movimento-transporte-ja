a59d24dab63093ac9c530d589fa5922e689b6dd9
Jamil Campeão <jamilcampeao@gmail.com>
2025-08-02 22:50:46 -0300

    fix: ajuste de fuso horário

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,11 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"options": "-c TimeZone=America/Sao_Paulo"}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
